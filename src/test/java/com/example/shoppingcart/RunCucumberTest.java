575003c116b1e4ec1006a5ceaee902b22c461b5c
davbaster <davbaster@gmail.com>
2024-05-18 18:06:21 -0600

    added new debug to runcucumbertest.java

--- src/test/java/com/example/shoppingcart/RunCucumberTest.java
+++ src/test/java/com/example/shoppingcart/RunCucumberTest.java
@@ -4,11 +4,19 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+import java.nio.file.*;
+import java.util.stream.*;
+
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", glue = "com.example.shoppingcart")
 public class RunCucumberTest {
     static {
-        // This line will print the current working directory to the console when tests are initialized
-        System.out.println("Current directory: " + System.getProperty("user.dir"));
+        try {
+            System.out.println("Current directory: " + System.getProperty("user.dir"));
+            Files.list(Paths.get("src/test/resources/features"))
+                 .forEach(path -> System.out.println(path.toString()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
