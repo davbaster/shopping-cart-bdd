c12fda5c8f65fc2d90a087d41a51609390cf8b26
davbaster <davbaster@gmail.com>
2024-05-18 18:00:33 -0600

    added a debug point in runcucumbertest.java

--- src/test/java/com/example/shoppingcart/RunCucumberTest.java
+++ src/test/java/com/example/shoppingcart/RunCucumberTest.java
@@ -7,4 +7,8 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", glue = "com.example.shoppingcart")
 public class RunCucumberTest {
+    static {
+        // This line will print the current working directory to the console when tests are initialized
+        System.out.println("Current directory: " + System.getProperty("user.dir"));
+    }
 }
